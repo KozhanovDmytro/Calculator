fbb98af61e15e156a70c21406bba7a4b62c63e66
KozhanovDmytro <33006436+KozhanovDmytro@users.noreply.github.com>
2018-09-13 22:10:55 +0300

    refactored

--- src/main/java/com/implemica/Main.java
+++ src/main/java/com/implemica/Main.java
@@ -3,9 +3,6 @@ package com.implemica;
 import com.implemica.view.Launcher;
 import javafx.application.Application;
 
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
-
 public class Main {
     public static void main(String[] args) {
         Application.launch(Launcher.class);
