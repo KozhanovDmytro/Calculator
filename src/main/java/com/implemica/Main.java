2adcdd7ee453805c04e2d89dedd3d633889f2b85
KozhanovDmytro <33006436+KozhanovDmytro@users.noreply.github.com>
2018-10-01 09:00:54 +0300

    added new tasks

--- src/main/java/com/implemica/Main.java
+++ src/main/java/com/implemica/Main.java
@@ -5,22 +5,19 @@ import javafx.application.Application;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Main {
     /*
     * Do it firstly.
     * TODO look at rounding mode. When you divide to number 3 so many times.
+    * TODO problem with not rounding mode. Use MathContext with your custom 'e'.
     * TODO 0.0000111111111111 when you write into label scale isn't 16 characters.
     * TODO 0.0000111111111111 * 0.1 must be with E.
     * TODO make History interface
     * TODO rename variable showOperand in Operand.
     */
     public static void main(String[] args) {
-        BigDecimal number = new BigDecimal("1");
-        for (int i = 0; i < 7; i++) {
-            number = number.divide(new BigDecimal(3), MathContext.DECIMAL64);
-            System.out.println(number);
-        }
-//        Application.launch(Launcher.class);
+        Application.launch(Launcher.class);
     }
 }
