237c6cc0951b0b52b5f07bd0bfba016872be4c42
KozhanovDmytro <33006436+KozhanovDmytro@users.noreply.github.com>
2018-10-23 17:59:17 +0300

    new idea for refactor resize action

--- src/main/java/com/implemica/view/util/NodesFinder.java
+++ src/main/java/com/implemica/view/util/NodesFinder.java
@@ -1,6 +1,7 @@
 package com.implemica.view.util;
 
 import lombok.Getter;
+import org.apache.commons.lang3.ObjectUtils;
 
 public enum NodesFinder {
    RESULT_LABEL("#resultLabel"),
@@ -15,6 +16,8 @@ public enum NodesFinder {
    EXTRA_INFO_FULL("#extraInfoFull"),
    EXTRA_INFO_BTNS("#extraInfoBtns"),
    EXTRA_MEMORY_LABEL("#extraMemoryLabel"),
+
+   /*panes for resize*/
    LEFT_RESIZE("#leftResize"),
    EXTRA_LEFT_RESIZE("#extraLeftResize"),
    RIGHT_RESIZE("#rightResize"),
@@ -29,15 +32,67 @@ public enum NodesFinder {
    RIGHT_BOTTOM_RESIZE_FULL("#rightBottomResizeFull"),
    BOTTOM_RESIZE_FULL("#bottomResizeFull");
 
-
-
-
-
-
    @Getter
    private String query;
 
    NodesFinder(String query) {
       this.query = query;
+   }
+
+   public Side getSide(NodesFinder nodesFinder) {
+      Side result;
+      switch (nodesFinder) {
+         case LEFT_RESIZE:
+            result = Side.LEFT;
+            break;
+         case EXTRA_LEFT_RESIZE:
+            result = Side.LEFT;
+            break;
+         case RIGHT_RESIZE:
+            result = Side.RIGHT;
+            break;
+         case EXTRA_RIGHT_RESIZE:
+            result = Side.RIGHT;
+            break;
+         case TOP_RESIZE:
+            result = Side.TOP;
+            break;
+         case BOTTOM_RESIZE:
+            result = Side.BOTTOM;
+            break;
+         case LEFT_BOTTOM_RESIZE:
+            result = Side.LEFT_BOTTOM;
+            break;
+         case RIGHT_BOTTOM_RESIZE:
+            result = Side.RIGHT_BOTTOM;
+            break;
+         case LEFT_TOP_RESIZE:
+            result = Side.LEFT_TOP;
+            break;
+         case RIGHT_TOP_RESIZE:
+            result = Side.RIGHT_TOP;
+            break;
+         case RIGHT_RESIZE_FULL:
+            result = Side.RIGHT;
+            break;
+         case RIGHT_BOTTOM_RESIZE_FULL:
+            result = Side.RIGHT_BOTTOM;
+            break;
+         case BOTTOM_RESIZE_FULL:
+            result = Side.BOTTOM;
+            break;
+         default:
+            throw new IllegalArgumentException();
+      }
+      return result;
+   }
+
+   public NodesFinder findByQuery(String query) {
+      for (NodesFinder node: NodesFinder.values()) {
+         if(node.getQuery().equals(query)){
+            return node;
+         }
+      }
+      throw new IllegalArgumentException();
    }
 }
